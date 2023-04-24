a3d677e1d45bf4e065ead9ba4ca0699ea5520434
Isha28 <isha.p285@gmail.com>
2023-04-24 15:46:25 -0500

    updated multiclass mlm

--- multiclass_mlm.py
+++ multiclass_mlm.py
@@ -96,8 +96,6 @@ def train_multiclass(args, train_df, eval_df, test_df, seed, model_configs):
     mlm_train_df = train_df.copy()
     mlm_train_df['text'] = mlm_train_df['text'].apply(lambda x: mlm_tokenizer.mask_token + ' ' + x)
     # mlm_train_df['labels'] = [-1]*len(mlm_train_df)
-    # mlm_train_df['labels'] = None
-    # mlm_train_df = mlm_train_df.drop('labels', axis=1)
 
     print ("PRINTING train_df ****** ", train_df)
     print ("PRINTING mlm_train_df ******* ", mlm_train_df)
