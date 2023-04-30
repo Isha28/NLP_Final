18d6746954172d83126e2a486326ba614aaff0e2
Isha28 <isha.p285@gmail.com>
2023-04-30 11:27:43 -0500

    pretrain mlm

--- pretrain_mlm.py
+++ pretrain_mlm.py
@@ -9,10 +9,10 @@ def pretrain_conflibert(model_path):
     model = BertForMaskedLM.from_pretrained(model_path)
 
     df = pd.read_csv('new_tweets_mlm.tsv', sep='\t')
-    text = df.values.tolist()
+    text = [sent for sublist in df.values.tolist() for sent in sublist]
     print("PRINT : Input Texts", text[:5])
     
-    inputs = tokenizer(text, return_tensors='pt', max_length=512, truncation=True, padding='max_length')
+    inputs = tokenizer(text, return_tensors='pt', max_length=32, truncation=True, padding='max_length')
 
     class MeditationsDataset(torch.utils.data.Dataset):
         def __init__(self, encodings):
@@ -26,9 +26,12 @@ def pretrain_conflibert(model_path):
 
     args = TrainingArguments(
         output_dir=model_path+'mlm_pretrain',
-        per_device_train_batch_size=16,
-        num_train_epochs=1, #change
-        evaluation_strategy='epoch'
+        per_device_train_batch_size=8,
+        num_train_epochs=10,
+        save_total_limit=1,
+        learning_rate=1e-4,
+        warmup_steps=1000,
+        fp16=True  # enable mixed precision training
     )
 
     data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm_probability=0.15)
@@ -46,10 +49,13 @@ def pretrain_conflibert(model_path):
     model.save_pretrained(model_path+'mlm_pretrain')
 
 def main():
+    torch.backends.cuda.max_split_size_mb = 200
+    torch.cuda.empty_cache()
+
     pretrain_conflibert('snowood1/ConfliBERT-scr-cased')
-    # pretrain_conflibert("snowood1/ConfliBERT-scr-uncased") #change
-    # pretrain_conflibert("snowood1/ConfliBERT-cont-cased")
-    # pretrain_conflibert("snowood1/ConfliBERT-cont-uncased")
+    pretrain_conflibert("snowood1/ConfliBERT-scr-uncased")
+    pretrain_conflibert("snowood1/ConfliBERT-cont-cased")
+    pretrain_conflibert("snowood1/ConfliBERT-cont-uncased")
 
 if __name__ == "__main__":
     main()
